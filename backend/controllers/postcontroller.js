1559d5f63acd6aa23d2ea3d4cc39f47a0fe625af
cod-op <shlokpatel094@gmail.com>
2026-09-07 15:04:59 +0530

    Fix case-sensitive model import

--- backend/controllers/postcontroller.js
+++ backend/controllers/postcontroller.js
@@ -1,4 +1,4 @@
-import Post from "../models/Post.js";
+import Post from "../models/post.js";
 import {uploadOnCloudinary } from "../config/cloudinary.js";
 
 // CREATE POST
--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.js";
-import Post from "../models/Post.js";
+import Post from "../models/post.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
